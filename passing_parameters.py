be886eec1d7641ba021f7a4dc45c66175485dc42
Gajendra Jung Katuwal <himalaya.jung@gmail.com>
2015-12-23 13:34:11 -0500

    multiplication with feed_dict

--- passing_parameters.py
+++ passing_parameters.py
@@ -1,11 +1,15 @@
 import tensorflow as tf
 
-x = tf.placeholder("float") # Create a symbolic variable 'a'
-y = tf.placeholder("float") # Create a symbolic variable 'b'
+x = tf.placeholder("float") # create a symbolic variable
+y = tf.placeholder("float")
 
-z = tf.add(x, y)
+z = tf.add(x, y) # create a symbolic expression
 
 sess = tf.Session() 
 
-print(sess.run(z, feed_dict={x: 1, y: 2})) # eval expressions with parameters for a and b
-print(sess.run(z, feed_dict={p: 3, q: 3})) # NameError: name 'p' is not defined
+print(sess.run(z, feed_dict={x: 3, y: 2})) # eval expressions with parameters x and y
+#print(sess.run(z, feed_dict={p: 3, q: 0})) # NameError: name 'p' is not defined
+
+print(sess.run(tf.mul(x,y), feed_dict={x: 3, y: 2})) # multiply
+
+print(sess.run(tf.mul(x,y), {x: 3, y: 2})) # multiply
